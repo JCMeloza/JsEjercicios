63e17b6db09006f0dea3954b009ce26b7cba9c65
JuanCarlos <melozajuancarlos@gmail.com>
2025-09-23 19:39:42 +0200

    Agregar archivos HTML y JavaScript para el ejercicio de cambiar color de fondo

---
+++ src/js/js_ejercicio1.js
@@ -0,0 +1,4 @@
+document.getElementById("cambiarColorBtn").addEventListener("click", () => {
+	const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+	document.body.style.backgroundColor = color;
+});
