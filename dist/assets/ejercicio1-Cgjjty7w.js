7e00cbdf89092ac61eed30b3d2805b919df30012
JuanCarlos <melozajuancarlos@gmail.com>
2025-10-04 11:43:33 +0200

    Actualización de rutas y configuración de Vite

---
+++ dist/assets/ejercicio1-Cgjjty7w.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-BQCNvzmp.js";document.getElementById(`cambiarColorBtn`).addEventListener(`click`,()=>{let e=`#`+Math.floor(Math.random()*16777215).toString(16);document.body.style.backgroundColor=e});
