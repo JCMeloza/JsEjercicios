ef3b24ece60d13a4bb764955be7f38b31859d8be
JuanCarlos <melozajuancarlos@gmail.com>
2025-10-04 11:42:50 +0200

    Actualizar la referencia del script en js_ejercicio_1.html y agregar configuración de build en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
 	base: "/JsEjercicios/",
 	server: {
 		watch: {
 			usePolling: true,
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				main: '/index.html',
+				ejercicio1: '/src/js_ejercicio_1.html',
+				ejercicio2: '/src/js_ejercicio_2.html',
+				ejercicio3: '/src/js_ejercicio_3.html',
+				ejercicio4: '/src/js_ejercicio_4.html',
+				ejercicio5: '/src/js_ejercicio_5.html',
+				ejercicio6: '/src/js_ejercicio_6.html',
+				ejercicio7: '/src/js_ejercicio_7.html',
+				ejercicio8: '/src/js_ejercicio_8.html',
+				ejercicio9: '/src/js_ejercicio_9.html',
+			},
 		},
 	},
 });
